ea912b161c93aa4af6f14b87fb27ee0fb4449269
Atul Tripathi <81318689+AtulTripathi01@users.noreply.github.com>
2022-07-13 02:31:53 +0530

    Time: 1 ms (62.90%), Space: 6.2 MB (8.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n<=1)
-            return n;
-        return fib(n-1) + fib(n-2);
+        double phi = (sqrt(5) + 1) / 2;     
+        return round(pow(phi, n) / sqrt(5));
     }
 };
