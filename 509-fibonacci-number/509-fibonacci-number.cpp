dbf05d9816def4bc73762d3be48a02bcc601d3b6
Atul Tripathi <81318689+AtulTripathi01@users.noreply.github.com>
2022-07-13 02:35:24 +0530

    Time: 4 ms (49.68%), Space: 6.1 MB (16.57%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -5,3 +5,10 @@ public:
         return round(pow(phi, n) / sqrt(5));
     }
 };
+
+/*int fib(int N) {
+        if(N == 0)  return 0;
+        if(N == 1)  return 1;
+        return fib(N-1) + fib(N-2);
+    }
+    */
